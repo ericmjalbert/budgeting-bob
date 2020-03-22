b1dfeec61b08934308bc6469a2736503ef548439
Eric Jalbert <ericmjalbert@gmail.com>
2020-03-22 16:38:27 -0400

    feat: Allow month selector for budget page

--- app/budgets.py
+++ app/budgets.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from datetime import date, datetime, timedelta
+from flask import Blueprint, render_template, request
 
 from .auth import login_required
 from .db import Database
@@ -6,15 +7,17 @@ from .db import Database
 
 bp = Blueprint("budgets", __name__)
 
+CURRENT_MONTH = datetime.now().strftime("%Y-%m-%d")
+
 
 @bp.route("/budgets")
 @login_required
 def budgets():
+    selected_month = request.args.get("selected_month") or CURRENT_MONTH
 
     with Database() as db:
-        # TODO Change the DATE_TRUNC to be based on a selector
         db.execute(
-            """
+            f"""
             SELECT
                 category,
                 budget,
@@ -32,11 +35,35 @@ def budgets():
             INNER JOIN public.categories AS ca
                 USING (category)
             WHERE category != 'transfer_between_accounts'
-                AND DATE_TRUNC('month', transaction_date)
-                    = DATE_TRUNC('month', CURRENT_DATE)
+                AND DATE_TRUNC('month', transaction_date) = '{selected_month}'
                 AND budget >= 0
             GROUP BY 1, 2
-        """)
+            """
+        )
         budget_rows = db.fetchall()
 
-    return render_template("budgets.html", rows=budget_rows)
+    available_months = get_available_months()
+
+    return render_template(
+        "budgets.html",
+        rows=budget_rows,
+        months=available_months,
+        selected_month=datetime.strptime(selected_month, "%Y-%m-%d")
+    )
+
+
+def get_available_months(starting_month=date(2020, 1, 1)):
+    """Get a list of all the months from starting_month to the current date.
+
+    This returns a list of date objects for each valid month.
+    """
+
+    now = datetime.now().date()
+    diff = now - starting_month
+
+    all_days = [starting_month + timedelta(days=i) for i in range(diff.days)]
+    month_trunc = [date.replace(day=1) for date in all_days]
+    unique_months = list(set(month_trunc))
+    ordered_months = sorted(unique_months, reverse=True)
+
+    return ordered_months
--- app/static/js/budgets.js
+++ app/static/js/budgets.js
@@ -1,5 +1,11 @@
 function main() {
-  // nothing for now...
+  $('#month-selector').change(function(){
+    // Get the month value that's is selected
+    var month = $(this).val()
+
+    // show budgets of selected month
+    location.href = '/budgets?selected_month=' + month
+  });
 }
 
 $(document).ready(function() {
