3a337bad86d75349e512286bd8354f6127aaeab1
Eric Jalbert <ericmjalbert@gmail.com>
2020-06-18 10:37:40 -0400

    feat: Change charting framework to am4chart for sliding daterange

--- app/static/js/account_totals.js
+++ app/static/js/account_totals.js
@@ -14,52 +14,71 @@ function getCumulativeValues(values) {
 }
 
 
-function renderChart(dates, values) {
-    const ctx = $('#myChart');
+function createAm4coreChart(dates, values) {
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
 
-    const data = {
-        // Labels should be Date objects
-        labels: dates,
-        datasets: [{
-            fill: false,
-            data: values,
-            borderColor: '#2299dd',
-            backgroundColor: '#2299dd',
-            lineTension: 0,
-        }]
+    // Create chart instance
+    var chart = am4core.create("myChart", am4charts.XYChart);
+
+    // Add data
+    chart.data = dates.map(function(date, i) {
+        return {"date": date, "value": values[i]}
+    });
+
+    // Create axes
+    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.minGridDistance = 50;
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.title.text = "Account Total CAD";
+    valueAxis.min = 0
+
+    // Create series
+    var series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.dateX = "date";
+    series.strokeWidth = 2;
+    series.fillOpacity = 0.6;
+    series.minBulletDistance = 10;
+    series.tooltipText = "{valueY}";
+    series.tooltip.pointerOrientation = "vertical";
+    series.tooltip.background.cornerRadius = 20;
+    series.tooltip.background.fillOpacity = 0.5;
+    series.tooltip.label.padding(12,12,12,12)
+
+    // Add scrollbar
+    chart.scrollbarX = new am4charts.XYChartScrollbar();
+    chart.scrollbarX.series.push(series);
+
+    // Add cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.xAxis = dateAxis;
+    chart.cursor.snapToSeries = series;
+}
+
+
+function generateChartData() {
+    var chartData = [];
+    var firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 1000);
+    var visits = 1200;
+    for (var i = 0; i < 500; i++) {
+        // we create date objects here. In your data, you can have date strings
+        // and then set format of your dates using chart.dataDateFormat property,
+        // however when possible, use date objects, as this will speed up chart rendering.
+        var newDate = new Date(firstDate);
+        newDate.setDate(newDate.getDate() + i);
+        
+        visits += Math.round((Math.random()<0.5?1:-1)*Math.random()*10);
+
+        chartData.push({
+            date: newDate,
+            visits: visits
+        });
     }
-
-    const options = {
-        type: 'line',
-        data: data,
-        options: {
-            fill: false,
-            responsive: true,
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Date",
-                    }
-                }],
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Account Total CAD",
-                    }
-                }]
-            }
-        }
-    }
-
-    Chart.defaults.global.legend.display = false;
-    const chart = new Chart(ctx, options);
+    return chartData;
 }
 
 
@@ -76,10 +95,13 @@ function main() {
             });
 
             cumulativeValues = getCumulativeValues(values);
-            
-            renderChart(dates, cumulativeValues);
+
+    //         
+    //         renderChart(dates, cumulativeValues);
+            createAm4coreChart(dates, cumulativeValues);
         }
     );
+
 }
 
 $(document).ready(function() {
