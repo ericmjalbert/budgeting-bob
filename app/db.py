df248ac69a4ec324f00e600859e8c1d5a5effba1
Eric Jalbert <ericmjalbert@gmail.com>
2020-06-18 17:10:09 -0400

    feat: Add database initilization for new apps

--- app/db.py
+++ app/db.py
@@ -28,9 +28,7 @@ class PandasConnection:
         self.engine = None
 
     def __enter__(self):
-        self.engine = sqlalchemy.create_engine(
-            current_app.config["DATABASE_URL"]
-        )
+        self.engine = sqlalchemy.create_engine(current_app.config["DATABASE_URL"])
         return self.engine
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -66,5 +64,103 @@ def read_from_db():
     with Database() as db:
         # Get count of rows
         db.execute("SELECT COUNT(*) AS rows FROM public.log_timestamp")
-        rows = db.fetchone()['rows']
+        rows = db.fetchone()["rows"]
     return rows
+
+
+def init_user_table():
+    with Database() as db:
+        sql = """
+            CREATE TABLE IF NOT EXISTS public.user (
+                id SERIAL PRIMARY KEY,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+            );
+        """
+        db.execute(sql)
+
+
+def init_categories_table():
+    with Database() as db:
+        sql = """
+            CREATE TABLE IF NOT EXISTS public.categories (
+                category TEXT PRIMARY KEY,
+                description TEXT,
+                budget INTEGER
+            );
+        """
+        db.execute(sql)
+
+        sql = """
+        INSERT INTO public.categories VALUES
+        ('athletics', 'Things that help us be more althetic', 500),
+        ('beauty', 'Things for beauty products or services', 500),
+        ('car', 'Money spent on things because we have and use the car', 1000),
+        ('cats', 'Things for the cats well-being', 500),
+        ('clothing', 'Items what we wear for the purpose of fashion or weather', 500),
+        ('dates', 'Money spent for date night activities or dinner', 500),
+        ('eating', 'Money spent on food for a single person', 500),
+        ('entertainment', 'Money spent on games or other entertaining things', 250),
+        ('gifts', 'Money spent on things for other external people', 250),
+        ('groceries', 'Money spent on grocery store items', 1000),
+        ('health', 'Things for physical/mental well-being', 250),
+        ('house_utilities', 'Money spent on regular house upkeep', 250),
+        ('household_goodies', 'Money spent to upgrade our house living', 250),
+        ('internet', 'Monthly Plan and Auxillary Purchases', 250),
+        ('pharmacy', 'Drug things that need a pharmacist', 250),
+        ('phones', 'Money spent on phone plans and auxilary phone items', 250),
+        ('rent', 'Money spent on house rent', 2000),
+        ('social', 'Money spent for activities with other people', 500),
+        ('work_expense', 'Money spent because I have a job', 500),
+
+        ('salary', 'Money earned from salary', -10000),
+        ('other_earnings', 'Money earned from other sources', 0),
+
+        ('transfer_between_accounts', 'Money that is transferred between accounts', 0),
+        ('PENDING', 'Category is currently undecided', 0)
+
+        ON CONFLICT DO NOTHING
+        """
+        db.execute(sql)
+
+
+def init_accounts_table():
+    with Database() as db:
+        sql = """
+            CREATE TABLE IF NOT EXISTS public.accounts (
+                number TEXT PRIMARY KEY,
+                type TEXT,
+                owner TEXT,
+                description TEXT,
+                initial_amount FLOAT,
+                liquidable BOOLEAN,
+                source_of_truth TEXT
+            );
+        """
+        db.execute(sql)
+
+        sql = """
+            INSERT INTO public.accounts VALUES
+            ('1111'), 'Savings', 'Bob', 'RBC Banking', 20000, TRUE, 'RBC'),
+            ('2222'), 'Visa', 'Bob', 'RBC Rewards VISA', 0, TRUE, 'RBC'),
+            ('3333'), 'Savings', 'Bobbette', 'RBC Banking', 50000, TRUE, 'RBC')
+            ON CONFLICT DO NOTHING
+            ;
+        """
+        db.execute(sql)
+
+
+def initialize_database():
+    with Database() as db:
+        sql = """
+        SELECT FROM pg_tables
+        WHERE schemaname = 'public'
+            AND tablename  = 'user'
+        """
+        db.execute(sql)
+        user_table_exists = (db.rowcount() == 1)
+
+    if not user_table_exists:
+        init_user_table()
+        init_categories_table()
+        init_accounts_table()
