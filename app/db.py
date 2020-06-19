796b9d3d09e868fb2be83983fe20bffc5c8d1b1f
Eric Jalbert <ericmjalbert@gmail.com>
2020-06-19 14:30:37 -0400

    chore: remove deprecated db functions

--- app/db.py
+++ app/db.py
@@ -33,36 +33,3 @@ class PandasConnection:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.engine.dispose()
-
-
-def gen_create_table_sql():
-    return """
-        CREATE TABLE IF NOT EXISTS public.log_timestamp (
-            id SERIAL PRIMARY KEY,
-            created TIMESTAMPTZ
-        )
-    """
-
-
-def gen_insert_table_sql(data):
-    return f"""
-        INSERT INTO public.log_timestamp (created)
-        VALUES ('{data}');
-    """
-
-
-def write_to_db(data):
-    with Database() as db:
-        # Create the table if it doesn't yet exist
-        db.execute(gen_create_table_sql())
-
-        # Insert data into table
-        db.execute(gen_insert_table_sql(data))
-
-
-def read_from_db():
-    with Database() as db:
-        # Get count of rows
-        db.execute("SELECT COUNT(*) AS rows FROM public.log_timestamp")
-        rows = db.fetchone()["rows"]
-    return rows
