4f6dd6f3b6943aade90eadda50d058ab81854415
Eric Jalbert <ericmjalbert@gmail.com>
2021-03-21 22:19:29 -0400

    feat: enable budget values to be changed through UI

---
+++ initialize_db_views.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+## This needs to be run on the postgres database after it has been initialized
+
+PSQL="heroku pg:psql --app budgeting-bob"
+
+##### Budget VIEWS
+$PSQL -c "
+CREATE VIEW public.monthly_budget
+AS
+
+WITH monthly_budget AS (
+    SELECT
+        ca.category,
+        month::DATE,
+        bu.budget,
+        SUM(CASE WHEN bu.budget IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY category ORDER BY month) as value_partition
+    FROM public.categories_new AS ca
+    CROSS JOIN GENERATE_SERIES('2020-01-01', CURRENT_DATE, '1 month') AS month
+    LEFT JOIN public.budgets AS bu
+        ON bu.category_id = ca.id
+        AND bu.updated_month = month::DATE
+)
+
+SELECT
+    category,
+    month,
+    FIRST_VALUE(budget) OVER (PARTITION BY category, value_partition ORDER BY month) AS budget
+FROM monthly_budget
+"
+
+
+$PSQL -c "
+CREATE VIEW public.monthly_spend
+AS
+
+SELECT
+    mb.category,
+    mb.month,
+    COALESCE(SUM(-1 * tr.value), 0) AS spend
+FROM public.monthly_budget AS mb
+LEFT JOIN public.transactions AS tr
+    ON tr.category = mb.category
+    AND DATE_TRUNC('month', tr.transaction_date) = mb.month
+GROUP BY 1, 2
+"
+
+
+$PSQL -c "
+CREATE VIEW public.cumulative_budget
+AS
+
+SELECT
+    category,
+    month,
+    SUM(budget) OVER (PARTITION BY category ORDER BY month) AS budget
+FROM public.monthly_budget
+"
+
+
+$PSQL -c "
+CREATE VIEW public.cumulative_spend
+AS
+
+SELECT
+    category,
+    month,
+    SUM(spend) OVER (PARTITION BY category ORDER BY month) AS spend
+FROM public.monthly_spend
+"
+
+
+
+$PSQL -c "
+CREATE VIEW public.monthly_remaining
+AS
+
+SELECT
+    mb.category,
+    mb.month,
+    mb.budget,
+    mb.budget - COALESCE(ms.spend, 0) AS remaining,
+    case
+        when mb.budget - COALESCE(ms.spend, 0) < -1 * mb.budget / 2 then 'very over budget'
+        when mb.budget - COALESCE(ms.spend, 0) < 0 then 'over budget'
+        when mb.budget - COALESCE(ms.spend, 0) >= 0 then 'under budget'
+        end as status,
+    -- need this column for bootstrap coloring
+    case
+        when mb.budget - COALESCE(ms.spend, 0) < -1 * mb.budget / 2 then 'table-danger'
+        when mb.budget - COALESCE(ms.spend, 0) < 0 then 'table-warning'
+        when mb.budget - COALESCE(ms.spend, 0) >= 0 then 'table-success'
+        end as status_class
+FROM public.monthly_budget AS mb
+LEFT JOIN public.monthly_spend AS ms
+    USING (category, month)
+WHERE mb.category != 'transfer_between_accounts'
+    AND mb.budget > 0
+"
